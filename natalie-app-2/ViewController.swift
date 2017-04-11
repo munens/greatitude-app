1ece9d7fa5f162076aab3d58ad6880be1c4a0d98
Kuzivakwashe Mutonga <Kuzi@Kuzivakwashes-MacBook-Pro.local>
2017-04-11 10:24:35 -0700

    moving to xcode 8: welcome page basic veiw created.

---
+++ natalie-app-2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  natalie-app-2
+//
+//  Created by Kuzivakwashe Mutonga on 2017-04-11.
+//  Copyright © 2017 Munene Kaumbutho - Evan Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
