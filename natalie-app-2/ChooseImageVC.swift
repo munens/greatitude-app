104ef0529315aed6a7095b23c11783953262110d
munens <kaumbuthom@yahoo.com>
2017-04-13 06:12:17 -0700

    added scroll view to choose image file.

---
+++ natalie-app-2/ChooseImageVC.swift
@@ -0,0 +1,65 @@
+//
+//  ChooseImageVC.swift
+//  natalie-app-2
+//
+//  Created by Kuzivakwashe Mutonga on 2017-04-13.
+//  Copyright © 2017 Munene Kaumbutho - Evan Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ChooseImageVC: UINavigationController {
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    var images = [UIImageView]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        
+        
+        //var scrollWidth = scrollView.frame.size.width
+        for x in 0...4 {
+            let image = UIImage(named: "icon\(x).jpg")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            newX = scrollView.frame.size.width / 2 + scrollView.frame.size.width * CGFloat(x)
+            
+            contentWidth += newX
+            
+            scrollView.addSubview(imageView)
+            
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 50)
+        }
+        
+        scrollView.backgroundColor = UIColor.black
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
