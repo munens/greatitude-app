8fa180aba8f80898d9d5b7ccbb3de73716c9e669
munens <kaumbuthom@yahoo.com>
2017-04-24 02:24:09 -0700

    added images to sign up, login and main pages. Added labels under images in the choose stock images page that also kighlight upon a stock image bing tapped.

--- natalie-app-2/ChooseImageVC.swift
+++ natalie-app-2/ChooseImageVC.swift
@@ -10,19 +10,22 @@ import UIKit
 
 class ChooseImageVC: UIViewController {
 
-    
     @IBAction func backButtonPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBOutlet weak var editButton: UIBarButtonItem!
+    
     @IBOutlet weak var scrollView: UIScrollView!
     var images = [UIImageView]()
+    
+    var themes = ["Ocean theme", "Medieval theme", "Black Pattern theme", "Rainbow Background theme", "Blue and Pink theme"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        
+        editButton.isEnabled = false
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -32,8 +35,21 @@ class ChooseImageVC: UIViewController {
         for x in 0...4 {
             
             let image = UIImage(named: "img\(x)")
+            let label = UILabel(frame: CGRect(x:0, y:240, width: 300, height: 30))
+            label.text = themes[x]
+            label.textAlignment = NSTextAlignment.center
             
             let imageView = UIImageView(image: image)
+            
+            imageView.layer.shadowColor = UIColor.black.cgColor
+            imageView.layer.shadowOpacity = 1
+            imageView.layer.shadowOffset = CGSize.zero
+            imageView.layer.shadowRadius = 10
+            //imageView.layer.shadowPath = UIBezierPath(rect: imageView.bounds).cgPath
+            //imageView.layer.shouldRasterize = true
+            
+            imageView.addSubview(label)
+            
             images.append(imageView)
             
             var newX: CGFloat = 0.0
@@ -58,7 +74,7 @@ class ChooseImageVC: UIViewController {
         
         //print("content Width 2: \(contentWidth)")
         
-        scrollView.backgroundColor = UIColor.black
+        //scrollView.backgroundColor = UIColor.black
         //scrollView.clipsToBounds = false
         
         scrollView.contentSize = CGSize(width: (contentWidth - 2500.0), height: 350)
@@ -68,11 +84,19 @@ class ChooseImageVC: UIViewController {
         //let tap_location = sender.location(in: scrollView)
         for image in images {
             image.layer.borderWidth = 0
+            let image_label = image.subviews.first as! UILabel
+            image_label.shadowColor = nil
+            image_label.textColor = UIColor.black
+            image_label.shadowOffset = CGSize(width: 0, height: 0)
         }
         let chosen_image = sender.view
         chosen_image?.layer.borderColor = UIColor.blue.cgColor
-        chosen_image?.layer.borderWidth = 4
-        
+        chosen_image?.layer.borderWidth = 6
+        let image_label = chosen_image?.subviews.first as! UILabel
+        image_label.textColor = UIColor.blue
+        image_label.shadowColor = UIColor.black
+        image_label.shadowOffset = CGSize(width: 0.5, height: 0.0)
+        editButton.isEnabled = true
     }
     
     
