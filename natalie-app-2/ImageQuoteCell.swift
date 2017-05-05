d3f55824a4493fc74f134fe38092a19476d13d3b
munens <kaumbuthom@yahoo.com>
2017-05-05 11:19:24 -0700

    added table view to main vc and create a view file for each cell.

---
+++ natalie-app-2/ImageQuoteCell.swift
@@ -0,0 +1,29 @@
+//
+//  ImageQuoteCell.swift
+//  natalie-app-2
+//
+//  Created by Munene Kaumbutho on 2017-05-05.
+//  Copyright © 2017 Munene Kaumbutho - Evan Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ImageQuoteCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    /*
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    */
+    
+    
+    
+
+}
